081f829d7220c91500c976f9c347e0b2aca1cf87
DuckiesLove <duckiesloves@outlook.com>
2025-07-10 17:26:36 -0700

    Update service worker cache version and cleanup

--- greenlight/sw.js
+++ greenlight/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'greenlight-v1';
+const CACHE = 'greenlight-v2';
 const FILES = [
   './',
   './index.html',
@@ -8,6 +8,13 @@ const FILES = [
 ];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
