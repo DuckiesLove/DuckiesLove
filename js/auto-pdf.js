2dd4f2b34f7a560c332157374894ad100bd6c450
DuckiesLove <duckiesloves@outlook.com>
2025-07-27 14:28:49 -0700

    Improve PDF export scaling

--- js/auto-pdf.js
+++ js/auto-pdf.js
@@ -1,18 +1,22 @@
 const exportToPDF = () => {
   const element = document.getElementById('pdf-container');
   const opt = {
-    margin: 0.5,
+    margin: 0,
     filename: 'Kink_Compatibility_Report.pdf',
     image: { type: 'jpeg', quality: 1 },
     html2canvas: {
-      scale: 2,
+      scale: 4,
       useCORS: true,
       backgroundColor: '#000',
       scrollY: 0,
-      windowWidth: document.body.scrollWidth,
-      windowHeight: document.body.scrollHeight
+      windowWidth: document.documentElement.scrollWidth,
+      windowHeight: document.documentElement.scrollHeight
     },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+    jsPDF: {
+      unit: 'px',
+      format: [document.documentElement.scrollWidth, document.documentElement.scrollHeight],
+      orientation: 'portrait'
+    },
     pagebreak: {
       mode: ['css', 'legacy'],
       avoid: ['.avoid-break']
