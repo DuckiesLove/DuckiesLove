c8f8bec1d28931c34fd12ba2721725dc49dfab7f
DuckiesLove <duckiesloves@outlook.com>
2025-07-27 15:41:47 -0700

    Improve auto-pdf export and remove extra sample

--- js/auto-pdf.js
+++ js/auto-pdf.js
@@ -1,20 +1,24 @@
 const exportToPDF = () => {
   const element = document.getElementById('pdf-container');
+  const width = element.scrollWidth;
+  const height = element.scrollHeight;
+
   const opt = {
     margin: 0,
     filename: 'Kink_Compatibility_Report.pdf',
     image: { type: 'jpeg', quality: 1 },
     html2canvas: {
-      scale: 4,
+      scale: 2,
       useCORS: true,
       backgroundColor: '#000',
       scrollY: 0,
-      windowWidth: document.documentElement.scrollWidth,
-      windowHeight: document.documentElement.scrollHeight
+      width,
+      windowWidth: width,
+      windowHeight: height
     },
     jsPDF: {
       unit: 'px',
-      format: [document.documentElement.scrollWidth, document.documentElement.scrollHeight],
+      format: [width, height],
       orientation: 'portrait'
     },
     pagebreak: {
