bdf425a9c9d6032933de4ecd6c2c4c9e96bf68d5
DuckiesLove <duckiesloves@outlook.com>
2025-09-13 15:17:26 -0700

    Replace role scoring with poly/mono aware module

--- test/roles.test.js
+++ test/roles.test.js
@@ -4,15 +4,14 @@ import { calculateRoleScores } from '../js/calculateRoleScores.js';
 
 test('calculateRoleScores maps answers to roles', () => {
   const survey = [
-    { question: 'Do you enjoy being restrained?', value: 5 },
-    { question: 'Do you like restraining others?', value: 3 },
-    { question: 'Do you enjoy acting bratty?', value: 4 }
+    { question: 'i enjoy rope', value: 5 },
+    { question: 'i enjoy being degraded', value: 4 },
+    { question: 'i am a brat', value: 5 }
   ];
   const results = calculateRoleScores(survey);
   const get = name => results.find(r => r.name === name).percent;
-  assert.strictEqual(get('Bondage Bottom'), 100);
   assert.strictEqual(get('Rope Bottom'), 100);
-  assert.strictEqual(get('Bondage Top'), 60);
-  assert.strictEqual(get('Rope Top'), 60);
-  assert.strictEqual(get('Brat'), 80);
+  assert.strictEqual(get('Rope Top'), 100);
+  assert.strictEqual(get('Brat'), 100);
+  assert.strictEqual(get('degradee'), 71);
 });
